8175dc534e8b28340bd3c35974b01e319b881132
PerfectSolution <PerfectSolution@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-13 21:29:03 +0000

    Push missing files
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-quickpay/tags/4.1.0@1180430 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/assets/javascript/backend.js
@@ -0,0 +1,166 @@
+( function( $ ) {
+	"use strict";
+
+	Quickpay.prototype.init = function() {
+		// Add event handlers
+		this.actionBox.on( 'click', '[data-action]', $.proxy( this.callAction, this ) );
+		this.actionBox.on( 'keyup', ':input[name="quickpay_split_amount"]', $.proxy( this.balancer, this ) );
+	};
+
+	Quickpay.prototype.callAction = function( e ) {
+		e.preventDefault();
+		var target = $( e.target );
+		var action = target.attr( 'data-action' );
+
+		if( typeof this[action] !== 'undefined' ) {
+			var message = target.attr('data-notify') || 'Are you sure you want to continue?';
+			if( confirm( message ) ) {
+				this[action]();	
+			}
+		}	
+	};
+
+	Quickpay.prototype.capture = function() {
+		var request = this.request( {
+			quickpay_action : 'capture'
+		} );
+	};
+
+	Quickpay.prototype.cancel = function() {
+		var request = this.request( {
+			quickpay_action : 'cancel'
+		} );
+	};
+
+	Quickpay.prototype.refund = function() {
+		var request = this.request( {
+			quickpay_action : 'refund'
+		} );
+	};
+
+	Quickpay.prototype.split_capture = function() {
+		var request = this.request( {
+			quickpay_action : 'splitcapture',
+			amount : parseFloat( $('#quickpay_split_amount').val() ),
+			finalize : 0
+		} );
+	};
+
+	Quickpay.prototype.split_finalize = function() {
+		var request = this.request( {
+			quickpay_action : 'splitcapture',
+			amount : parseFloat( $('#quickpay_split_amount').val() ),
+			finalize : 1
+		} );
+	};
+
+	Quickpay.prototype.request = function( dataObject ) {
+		var that = this;
+		var request = $.ajax( {
+			type : 'POST',
+			url : ajaxurl,
+			dataType: 'json',
+			data : $.extend( {}, { action : 'quickpay_manual_transaction_actions', post : this.postID.val() }, dataObject ),
+			beforeSend : $.proxy( this.showLoader, this, true ),
+			success : function() {
+				$.get( window.location.href, function( data ) {
+					var newData = $(data).find( '#' + that.actionBox.attr( 'id' ) + ' .inside' ).html();
+					that.actionBox.find( '.inside' ).html( newData );
+					that.showLoader( false );
+				} );
+			}
+		} );
+
+		return request;
+	};
+
+	Quickpay.prototype.showLoader = function( e, show ) {
+		if( show ) {
+			this.actionBox.append( this.loaderBox );
+		} else {
+			this.actionBox.find( this.loaderBox ).remove();
+		}
+	};
+
+	Quickpay.prototype.balancer = function(e) {
+		var remainingField = $('.quickpay-remaining');
+		var balanceField = $('.quickpay-balance');
+		var amountField = $(':input[name="quickpay_split_amount"]');
+		var btnCaptureSplit = $('#quickpay_split_button');
+		var btnSplitFinalize = $('#quickpay_split_finalize_button');
+		var amount = parseFloat(amountField.val().replace(',','.'));
+
+		if( amount > parseFloat(remainingField.text()) || amount <= 0 || isNaN(amount) || amount == '') {
+			amountField.addClass('warning');
+			btnCaptureSplit.fadeOut().prop('disabled', true);
+			btnSplitFinalize.fadeOut().prop('disabled', true);
+		} else {
+			amountField.removeClass('warning');
+			btnCaptureSplit.fadeIn().prop('disabled', false);
+			btnSplitFinalize.fadeIn().prop('disabled', false);
+		}
+	};
+    
+    
+    QuickpayAjaxStatus.prototype.init = function() {
+        var self = this;
+        if (this.elements.length) {
+            this.elements.each(function() {
+                var $this = $(this);
+
+                var data = {};
+                
+                data['quickpay-transaction-id'] = $this.data('quickpay-transaction-id');
+                data['quickpay-post-id'] = $this.data('quickpay-post-id');
+
+                self.ajaxGetTransaction(data).done(function(transactionData){
+                    self.handleTransactionData($this, transactionData);
+                });
+         
+            });
+        }
+    };
+    
+    QuickpayAjaxStatus.prototype.handleTransactionData = function($element, transactionData) {
+        $.each(transactionData, function(key, data) {
+            var contentElement = $element.find('[data-quickpay-show="' + key + '"]');
+                contentElement.text(data.value);
+            
+            if (data.hasOwnProperty('attr')) {
+                $.each(data.attr, function(attr, attrValue) {
+                    if(!!contentElement.attr(attr)) {
+                        attrValue += ' ' + contentElement.attr(attr);  
+                    }
+                    contentElement.attr(attr, attrValue); 
+                });
+            }
+            
+            $element.removeClass('quickpay-loader');
+        });
+    };
+    
+    QuickpayAjaxStatus.prototype.ajaxGetTransaction = function(data) {
+        var promise = $.Deferred();
+        $.getJSON(ajaxurl, $.extend( {}, { action : 'quickpay_get_transaction_information' }, data ), function(response) {
+           promise.resolve(response); 
+        });
+        return promise;
+    };
+    
+	// DOM ready
+	$(function() {
+		new Quickpay().init();
+        new QuickpayAjaxStatus().init();
+	});
+
+	function Quickpay() {
+		this.actionBox 	= $( '#quickpay-payment-actions' );
+		this.postID		= $( '#post_ID' );
+		this.loaderBox 	= $( '<div class="loader"></div>');
+	}
+    
+    function QuickpayAjaxStatus() {
+        this.elements = $( '[data-quickpay-transaction-id]' );
+    }
+
+})(jQuery);
---
+++ classes/instances/paii.php
@@ -0,0 +1,79 @@
+<?php
+
+class WC_Quickpay_Paii extends WC_Quickpay_Instance {
+    
+    public $main_settings = NULL;
+    
+    public function __construct() {
+        parent::__construct();
+        
+        // Get gateway variables
+        $this->id = 'Paii';
+        
+        $this->setup();
+        
+        $this->title = $this->s('title');
+        $this->description = $this->s('description');
+        
+        add_filter( 'woocommerce_quickpay_cardtypelock_paii', array( $this, 'filter_cardtypelock' ) );
+    }
+
+    
+    /**
+    * init_form_fields function.
+    *
+    * Initiates the plugin settings form fields
+    *
+    * @access public
+    * @return array
+    */
+    public function init_form_fields()
+    {
+        $this->form_fields = array(
+            'enabled' => array(
+                'title' => __( 'Enable/Disable', 'woo-quickpay' ), 
+                'type' => 'checkbox', 
+                'label' => __( 'Enable Paii payment', 'woo-quickpay' ), 
+                'default' => 'no'
+            ), 
+            '_Shop_setup' => array(
+                'type' => 'title',
+                'title' => __( 'Shop setup', 'woo-quickpay' ),
+            ),
+                'title' => array(
+                    'title' => __( 'Title', 'woo-quickpay' ), 
+                    'type' => 'text', 
+                    'description' => __( 'This controls the title which the user sees during checkout.', 'woo-quickpay' ), 
+                    'default' => __('Paii', 'woo-quickpay')
+                ),
+                'description' => array(
+                    'title' => __( 'Customer Message', 'woo-quickpay' ), 
+                    'type' => 'textarea', 
+                    'description' => __( 'This controls the description which the user sees during checkout.', 'woo-quickpay' ), 
+                    'default' => __('Pay with your mobile phone', 'woo-quickpay')
+                ),
+                'quickpay_paybuttontext' => array(
+                    'title' => __( 'Text on payment button', 'woo-quickpay' ), 
+                    'type' => 'text', 
+                    'description' => __( 'This text will show on the button which opens the Quickpay window.', 'woo-quickpay' ), 
+                    'default' => __( 'Open secure Quickpay window.', 'woo-quickpay' )
+                ),
+        );
+    }
+   
+    
+    /**
+    * filter_cardtypelock function.
+    *
+    * Sets the cardtypelock
+    *
+    * @access public
+    * @param  string - ID of the payment gateway chosen for order payment
+    * @param  string - The cardtypelock from settings
+    * @return string
+    */
+    public function filter_cardtypelock()
+    {
+        return 'paii';
+    }
+}
